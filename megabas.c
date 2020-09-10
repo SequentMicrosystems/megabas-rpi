32d25fcdd9437fd6e0f4c7bae291bd604544ec68
alexburcea2877 <olcitu@gmail.com>
2020-09-10 13:19:43 +0000

    Add RTC support, valid with firmware version >= 1.04

--- megabas.c
+++ megabas.c
@@ -2268,6 +2268,128 @@ static void doRs485Write(int argc, char *argv[])
 	}
 }
 
+static void doRTCGet(int argc, char *argv[]);
+const CliCmdType CMD_RTC_GET =
+	{
+		"rtcrd",
+		2,
+		&doRTCGet,
+		"\trtcrd:		Get the internal RTC  date and time(mm/dd/yy hh:mm:ss)\n",
+		"\tUsage:		megaind <id> rtcrd \n",
+		"",
+		"\tExample:	megaind 0 rtcrd; Get the nternal RTC time and date on Board #0\n"};
+
+static void doRTCGet(int argc, char *argv[])
+{
+	int dev = 0;
+	u8 buff[6];
+
+	dev = doBoardInit(atoi(argv[1]));
+	if (dev <= 0)
+	{
+		exit(1);
+	}
+
+	if (argc == 3)
+	{
+		if (OK != i2cMem8Read(dev, I2C_RTC_YEAR_ADD, buff, 6))
+		{
+			printf("Fail to read RTC!\n");
+			exit(1);
+		}
+
+		printf("%02u/%02u/%02u %02u:%02u:%02u\n", buff[1], buff[2], buff[0],
+			buff[3], buff[4], buff[5]);
+	}
+	else
+	{
+		printf("Invalid params number:\n %s", CMD_RTC_GET.usage1);
+		exit(1);
+	}
+}
+
+static void doRTCSet(int argc, char *argv[]);
+const CliCmdType CMD_RTC_SET =
+	{
+		"rtcwr",
+		2,
+		&doRTCSet,
+		"\trtcwr:		Set the internal RTC  date and time(mm/dd/yy hh:mm:ss)\n",
+		"\tUsage:		megaind <id> rtcwr <mm> <dd> <yy> <hh> <mm> <ss> \n",
+		"",
+		"\tExample:	megaind 0 rtcwr 9 15 20 21 43 15; Set the internal RTC time and date on Board #0 at Sept/15/2020  21:43:15\n"};
+
+static void doRTCSet(int argc, char *argv[])
+{
+	int dev = 0;
+	u8 buff[7];
+	int i = 0;
+
+	dev = doBoardInit(atoi(argv[1]));
+	if (dev <= 0)
+	{
+		exit(1);
+	}
+
+	if (argc == 9)
+	{
+		i = atoi(argv[3]);//month
+		if(i < 1 || i > 12)
+		{
+			printf("Invalid month!\n");
+			exit(1);
+		}
+		buff[1] = i;
+		i = atoi(argv[4]);
+		if(i < 1 || i > 31)
+		{
+			printf("Invalid date!\n");
+			exit(1);
+		}
+		buff[2] = i;
+		i = atoi(argv[5]);
+		if(i < 0 || i > 99)
+		{
+			printf("Invalid year!\n");
+			exit(1);
+		}
+		buff[0] = i;
+		i = atoi(argv[6]);
+		if(i < 0 || i > 23)
+		{
+			printf("Invalid hour!\n");
+			exit(1);
+		}
+		buff[3] = i;
+		i = atoi(argv[7]);
+		if(i < 0 || i > 59)
+		{
+			printf("Invalid minute!\n");
+			exit(1);
+		}
+		buff[4] = i;
+		i = atoi(argv[8]);
+		if(i < 0 || i > 59)
+		{
+			printf("Invalid second!\n");
+			exit(1);
+		}
+		buff[5] = i;
+		buff[6] = CALIBRATION_KEY;
+		if (OK != i2cMem8Write(dev, I2C_RTC_SET_YEAR_ADD, buff, 7))
+		{
+			printf("Fail to read RTC!\n");
+			exit(1);
+		}
+		printf("done\n");
+	}
+	else
+	{
+		printf("Invalid params number:\n %s", CMD_RTC_SET.usage1);
+		exit(1);
+	}
+}
+
 const CliCmdType* gCmdArray[] =
 {
 	&CMD_VERSION,
@@ -2301,6 +2423,8 @@ const CliCmdType* gCmdArray[] =
 	&CMD_WDT_GET_OFF_PERIOD,
 	&CMD_RS485_READ,
 	&CMD_RS485_WRITE,
+	&CMD_RTC_GET,
+	&CMD_RTC_SET,
 	NULL}; //null terminated array of cli structure pointers
 
 int main(int argc, char *argv[])
